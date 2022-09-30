081916edb5ff10d4893bc4a87364bbb6ec52c2f1
Oleksanr Ilchuk <o.ilchuk.2000@gmail.com>
2022-09-30 20:44:10 +0300

    Added fabrics

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,14 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\PostTag;
+use App\Models\PostUserLike;
+use App\Models\Tag;
+use App\Models\User;
+use Database\Factories\PostTagFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +22,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Category::factory(15)->create();
+        //Tag::factory(20)->create();
+        //Post::factory(50)->create();
+        //PostTag::factory(100)->create();
+        //User::factory(5)->create();
+        //PostUserLike::factory(30);
+        //Comment::factory(25);
     }
 }
